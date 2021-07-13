8b572e74be4107e46d781617f9dbefa070bf435b
xaverstribl <xaver.stribl@t-online.de>
2021-07-14 01:03:43 +0200

    splitter now super fast, splitter2 not needed anymore

--- Splitter.sh
+++ Splitter.sh
@@ -2,32 +2,40 @@
 
 function Splitter {
 
-	## check that given argument is a existing directory
-	[[ $# -eq 1 ]] || { echo "Error: wrong number of arguments"; return 1; }
-	[[ -d ${1} ]] || { echo "Error: given directory does not exist"; return 2; }
+	## variables
+	local topDir=$(realpath ${1})
+	local eventLines
+	local lastLine
+	local dir
 	
-	## split into seperate files
-	## 1
-	(
-	cd ${1}
-	cd ./0/
+	while read dir; do
 	
-	for i in 0 1 2 3 4 5 6 7 8 9; do
-		lines=($(echo $(grep -n BEGINNINGOFEVENT HIJING_LBF_test_small.out | awk 'BEGIN {FS=":"}{print $1}') | awk '{print $i}'))
-	done
+		## start subshell
+		(
+		
+		## get lines of events
+		eventLines=($(echo $(grep -n BEGINNINGOFEVENT ${dir}/HIJING_LBF_test_small.out | awk 'BEGIN {FS=":"}{print $1}') | awk '{print $i}'))
+		
+		## get the last line of the file
+		lastLine=$(wc -l ${dir}/HIJING_LBF_test_small.out | awk '{print $1}')
+		
+		## split into files
+		sed -n ${eventLines[0]},${eventLines[1]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_0.dat
+		sed -n ${eventLines[1]},${eventLines[2]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_1.dat
+		sed -n ${eventLines[2]},${eventLines[3]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_2.dat
+		sed -n ${eventLines[3]},${eventLines[4]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_3.dat
+		sed -n ${eventLines[4]},${eventLines[5]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_4.dat
+		sed -n ${eventLines[5]},${eventLines[6]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_5.dat
+		sed -n ${eventLines[6]},${eventLines[7]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_6.dat
+		sed -n ${eventLines[7]},${eventLines[8]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_7.dat
+		sed -n ${eventLines[8]},${eventLines[9]}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_8.dat
+		sed -n ${eventLines[9]},${lastLine}p ${dir}/HIJING_LBF_test_small.out > ${dir}/event_9.dat
+		
+		) &
+		
+	done < <(find ${topDir} -mindepth 1 -maxdepth 1 -type d)
 	
-	local last_line=$(wc -l HIJING_LBF_test_small.out | awk '{print $1}')
 	
-	sed -n ${lines[0]},${lines[1]}p HIJING_LBF_test_small.out > event_0.dat
-	sed -n ${lines[1]},${lines[2]}p HIJING_LBF_test_small.out > event_1.dat
-	sed -n ${lines[2]},${lines[3]}p HIJING_LBF_test_small.out > event_2.dat
-	sed -n ${lines[3]},${lines[4]}p HIJING_LBF_test_small.out > event_3.dat
-	sed -n ${lines[4]},${lines[5]}p HIJING_LBF_test_small.out > event_4.dat
-	sed -n ${lines[5]},${lines[6]}p HIJING_LBF_test_small.out > event_5.dat
-	sed -n ${lines[6]},${lines[7]}p HIJING_LBF_test_small.out > event_6.dat
-	sed -n ${lines[7]},${lines[8]}p HIJING_LBF_test_small.out > event_7.dat
-	sed -n ${lines[8]},${lines[9]}p HIJING_LBF_test_small.out > event_8.dat
-	sed -n ${lines[9]},${last_line}p HIJING_LBF_test_small.out > event_9.dat
-	)
-	
+	return 0
+
 }
