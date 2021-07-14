78152ba3c96a3daaf774c98093fe134e3ed72d83
xaverstribl <xaver.stribl@t-online.de>
2021-07-14 02:50:04 +0200

    filter works now, but wait does not

--- Filter.sh
+++ Filter.sh
@@ -2,6 +2,35 @@
 
 function Filter {
 
+	## variables
+	local topDir=$(realpath ${1})
+	local file
+	local backup
+
+	while read file; do
 	
+		echo "cycle:" ${counter}
+	
+ 		## go to the directory of the current file
+ 		cd $(dirname ${file})
+		
+		## start subshells
+		(
+		echo "Subshell PID: ${BASHPID}"
+		backup=$( sed "s/event/backup/" <(echo ${file}))
+		cp ${file} ${backup} 
+		awk '{if ($3 == 0) print $0}' < ${backup} 1>${file}
+		) &
+		
+		## go back
+ 		cd -
+ 		
+	done < <(find ${topDir} -type f -name "event_*.dat")
+	
+	## remove backup files again
+	rm $(find ${topDir} -type f -name "backup_*.dat")
+	
+	#wait
+	return 0
 
 }
